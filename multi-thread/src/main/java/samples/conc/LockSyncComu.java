18aa2f4dac64fccbbae899d67ab1c4ae68e0b0c4
wangyongtao <wangyt4dev@gmail.com>
2019-09-20 11:04:48 +0800

    improve the multi thread sample.

---
+++ multi-thread/src/main/java/samples/conc/LockSyncComu.java
@@ -0,0 +1,85 @@
+package samples.conc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Lock & Condition线程同步通信技术
+ *
+ * <p>子线程打印10次，接着主线程打印100次...循环如此50次。
+ *
+ * <p>wait() notify()
+ */
+public class LockSyncComu {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    final Biz biz = new Biz();
+
+    Runnable subr =
+        () -> {
+          for (int i = 0; i < 50; i++) {
+            try {
+              biz.sub(i);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+        };
+
+    /* 子线程 */
+    new Thread(subr).start();
+
+    /* 主线程 */
+    for (int i = 0; i < 50; i++) {
+      biz.main(i);
+    }
+  }
+
+  static class Biz {
+
+    boolean onoff = true;
+
+    final Lock lock = new ReentrantLock();
+    final Condition cond = lock.newCondition();
+
+    public void sub(int i) throws InterruptedException {
+      try {
+        lock.lock();
+
+        while /*if*/ (!onoff) { // 应该使用while，而不应该使用if，
+          cond.await(); // 执行到该行导致线程等待时，线程有可能被cpu虚假唤醒；如果唤醒，上面代码为if，会导致执行继续后续的代码；如果为while，则会继续循环判断条件。
+        }
+
+        for (int j = 0; j < 10; j++) {
+          System.out.println(String.format("sub thread sequence of %d ,loop of %d", j, i));
+        }
+
+        onoff = false; // 退出该方法后，如果子线程继续执行，则让进入while等待。
+        cond.signal(); // 通知主线程执行
+      } finally {
+        lock.unlock();
+      }
+    }
+
+    public void main(int i) throws InterruptedException {
+      try {
+        lock.lock();
+
+        while /*if*/ (onoff) { // 应该使用while，而不应该使用if，
+          cond.await(); // 执行到该行导致线程等待时，线程有可能被cpu虚假唤醒；如果唤醒，上面代码为if，会导致执行继续后续的代码；如果为while，则会继续循环判断条件。
+        }
+
+        for (int j = 0; j < 100; j++) {
+          System.out.println(String.format("main thread sequence of %d ,loop of %d", j, i));
+        }
+
+        onoff = true; // 退出该方法后，如果主线程继续执行，则进入while等待
+        cond.signal(); // 通知子线程执行
+      } finally {
+        lock.unlock();
+      }
+    }
+  }
+}
