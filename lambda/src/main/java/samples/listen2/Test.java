1ee4f4e34749178f330b0d97cb97b8f6ca941983
wangyongtao <wangyt4dev@gmail.com>
2019-09-27 15:33:57 +0800

    .

---
+++ lambda/src/main/java/samples/listen2/Test.java
@@ -0,0 +1,10 @@
+package samples.listen2;
+
+public class Test {
+
+  public static void main(String[] args) {
+    CustomerService afterSale = new CustomerService();
+    Customer consumer = new Customer(afterSale);
+    consumer.consult("流量使用情况");
+  }
+}
