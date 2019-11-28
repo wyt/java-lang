9872c38be4d42c7ad4f488ef155ffefcfa146e29
wangyongtao <wangyt4dev@gmail.com>
2019-11-28 14:09:52 +0800

    .

--- java8-new-features/src/main/java/samples/stream/StreamTest01.java
+++ java8-new-features/src/main/java/samples/stream/StreamTest01.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * https://www.runoob.com/java/java8-streams.html<br>
  * Stream（流）是一个来自数据源的元素队列并支持聚合操作
  *
  * @author wangyongtao
  * @date 2019/11/24
+ * @refer https://www.ibm.com/developerworks/cn/java/j-lo-java8streamapi
  */
 public class StreamTest01 {
 
@@ -17,9 +17,20 @@ public class StreamTest01 {
 
     //  为集合创建串行流
     List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+
     List<String> filtered =
         strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
-
     filtered.forEach(System.out::println);
+
+    String r =
+        strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(","));
+    System.out.println(r);
+
+    List<String> maped =
+        strings.stream()
+            .filter(string -> !string.isEmpty())
+            .map(String::toUpperCase)
+            .collect(Collectors.toList());
+    maped.forEach(System.out::println);
   }
 }
