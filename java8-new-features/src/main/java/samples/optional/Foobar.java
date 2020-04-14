b117c5d9bcaeaa15f201f640564640f2e4e09f73
wangyongtao <wangyt4dev@gmail.com>
2020-04-14 10:33:13 +0800

    opt.

--- java8-new-features/src/main/java/samples/optional/Foobar.java
+++ java8-new-features/src/main/java/samples/optional/Foobar.java
@@ -1,9 +1,40 @@
 package samples.optional;
 
+import java.util.Optional;
+
 /**
+ * Optional用例
+ *
  * @author wangyongtao
  * @date 2019/12/21
  */
 public class Foobar {
 
+  public static void main(String[] args) {
+    String strA = " abcd ", strB = null;
+    print(strA);
+    print("");
+    print(strB);
+    getLength(strA);
+    getLength("");
+    getLength(strB);
+  }
+
+  public static int getLength(String text) {
+    // Pre-Java 8
+    // return if (text != null) ? text.length() : -1;
+
+    // Java 8
+    return Optional.ofNullable(text).map(String::length).orElse(-1);
+  }
+
+  public static void print(String text) {
+    //    // Pre-Java 8
+    //    if (text != null) {
+    //      System.out.println(text);
+    //    }
+
+    // Java 8
+    Optional.ofNullable(text).ifPresent(System.out::println);
+  }
 }
